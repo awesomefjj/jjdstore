b677c3a5f9f863555c9656f87227aa74a45184e4
Jeffery <522023893@qq.com>
2021-04-30 15:32:09 +0800

    v1.0

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,7 @@ class GroupsController < ApplicationController
      @group = Group.new(group_params)
      @group.user = current_user
      if @group.save
+      current_user.join!(@group)
        redirect_to groups_path
      else
        render :new
@@ -41,6 +42,30 @@ class GroupsController < ApplicationController
           flash[:alert] = "Group deleted"
           redirect_to groups_path
       
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本论坛版成功"
+    else
+      flash[:warning] = "你已经是本讨论版成员了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本论坛版"
+    else
+      flash[:warning] = "本来就不是哦" 
+    end
+    redirect_to group_path(@group)
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,18 @@ class User < ApplicationRecord
   has_many :groups
   has_many :posts
 
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
+#判断自己在不在群组里
+ def is_member_of?(group)
+   participated_groups.include?(group)
+ end
 
-
+ def join!(group)
+  participated_groups << group
+ end
+ 
+ def quit!(group)
+  participated_groups.delete(group)
+ end
 end
