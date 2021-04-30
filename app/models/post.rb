ab0fdcf8cbcd5560eb0837dab540c814a680df7c
Jeffery <522023893@qq.com>
2021-04-30 14:36:32 +0800

    分页功能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
     validates :content, presence: true
     belongs_to :user
     belongs_to :group
+    #Rails 内建的一个API scope可以用来包装一些查询式，
+    #可以直接在post_controller 中使用recent
+    scope :recent, -> {order("created_at DESC")}
 end
